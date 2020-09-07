d3c2555284ab11d80b99d45cc812be7c52fecaa9
Kanishkan Kukarajah <kanishkan77@gmail.com>
2020-09-07 16:42:19 -0400

    Add search bar using paper and inputbase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -8,7 +8,7 @@ const AppRoutes = () => (
   <div>
     <Header/>
     <Switch>
-      <Route exact='/' component={Home}/>
+      <Route exact path='/' component={Home}/>
     </Switch>
   </div>
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,38 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { Component } from 'react';
+import { Typography, makeStyles, Paper, InputBase, IconButton } from '@material-ui/core';
+// import { SearchIcon } from '@material-ui/icons/Search';
+// import { Link } from 'react-router-dom';
 
-export default () => (
-    <Typography variant="h4">Seach for a movie!</Typography>
-);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        margin: '8px',
+        alignItems: 'center',
+        width: 400,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+    iconButton: {
+        padding: 10,
+    },
+}));
+
+export default function Home() {
+    const styles = useStyles();
+
+    return (
+        <div>
+            {/* <Typography variant="h4">Seach for a movie!</Typography> */}
+            <Paper component="form" className={styles.root}>
+                <InputBase 
+                    className={styles.input} 
+                    placeholder="Seach for a movie" 
+                    inputProps={{'aria-label': 'search for a movie'}}
+                />
+            </Paper> 
+        </div>
+    )
+}
